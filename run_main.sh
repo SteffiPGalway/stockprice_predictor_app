0c4e3e31e2bdbfb2323ba45deb538fba83de86ab
SteffiPGalway <steffipoliwoda@gmail.com>
2025-07-06 17:15:02 +0200

    preparing for render deployment

--- run_main.sh
+++ run_main.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# gunicorn run:app --bind 0.0.0.0:8080 --reload --log-level debug
-web: gunicorn run:app --bind 0.0.0.0:$PORT --reload --log-level debug
+gunicorn run:app --bind 0.0.0.0:8080 --reload --log-level debug
 
