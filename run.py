9d0c60eabcf0d54911a0cc83bc3f97a54133a401
SteffiPGalway <steffipoliwoda@gmail.com>
2025-07-06 16:59:44 +0200

    deploying to render

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Starting Flask on port {port}")
+    app.run(host="0.0.0.0", port=port)
     # app.run(debug=True)
--- run_main.sh
+++ run_main.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gunicorn run:app --bind 0.0.0.0:8080 --reload --log-level debug
+# gunicorn run:app --bind 0.0.0.0:8080 --reload --log-level debug
+web: gunicorn run:app --bind 0.0.0.0:$PORT --reload --log-level debug
+
